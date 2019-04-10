b57340d44736f17eea36860fbfbf7d65954ea080
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-04-10 15:55:02 +0800

    Initial Commit for Base Library

---
+++ src/test/kotlin/com/flatxph/base/BaseApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.flatxph.base
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
