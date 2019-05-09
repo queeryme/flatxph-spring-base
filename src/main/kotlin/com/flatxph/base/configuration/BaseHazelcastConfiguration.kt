fe495dc3247f5fd780be90979c83a78cefb891e6
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-05-09 08:46:54 +0800

    Tried to add hazelcast config again for brevity

--- src/main/kotlin/com/flatxph/base/configuration/BaseHazelcastConfiguration.kt
+++ src/main/kotlin/com/flatxph/base/configuration/BaseHazelcastConfiguration.kt
@@ -4,19 +4,34 @@ import com.hazelcast.config.Config
 import com.hazelcast.config.EvictionPolicy
 import com.hazelcast.config.MapConfig
 import com.hazelcast.config.MaxSizeConfig
+import org.springframework.beans.factory.annotation.Value
 
-fun buildHazelcastMapConfig(): MapConfig {
-    return MapConfig().apply {
+/**
+ * Override hazelcastConfiguration for auto hazelcast configuration.
+ * It should look like this:
+ * ```
+ * @EnableCaching
+ * @Configuration
+ * class HazelcastConfiguration {
+ *      @Bean
+ *      override fun hazelcastConfiguration(): Config = super.hazelcastConfiguration()
+ * }
+ * ```
+ */
+abstract class BaseHazelcastConfiguration {
+
+    @Value("\${spring.application.name}")
+    protected val applicationName: String? = null
+
+    open fun buildMapConfig() = MapConfig().apply {
         name = "configuration"
         maxSizeConfig = MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE)
         evictionPolicy = EvictionPolicy.LRU
         timeToLiveSeconds = 3600
     }
-}
 
-fun buildHazelcastConfiguration(applicationName: String): Config {
-    return Config().apply {
+    open fun buildHazelcastConfig() = Config().apply {
         instanceName = applicationName
-        addMapConfig(buildHazelcastMapConfig())
+        addMapConfig(buildMapConfig())
     }
 }
