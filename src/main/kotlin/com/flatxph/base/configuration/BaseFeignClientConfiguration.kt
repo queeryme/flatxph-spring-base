9397dfee2d723687e9ff94471e07072d58606cfc
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-05-12 22:03:40 +0800

    Fixed Final Base Feign Client Configuration Class to abstract to allow inheritance

--- src/main/kotlin/com/flatxph/base/configuration/BaseFeignClientConfiguration.kt
+++ src/main/kotlin/com/flatxph/base/configuration/BaseFeignClientConfiguration.kt
@@ -3,13 +3,14 @@ package com.flatxph.base.configuration
 import com.flatxph.base.security.SecurityUtils
 import feign.RequestInterceptor
 import org.springframework.http.HttpHeaders
+import com.flatxph.base.security.JwtConstants.PREFIX
 
-class BaseFeignClientConfiguration {
+abstract class BaseFeignClientConfiguration {
 
     fun buildBaseFeignClientConfiguration() = RequestInterceptor {
         template -> SecurityUtils.currentUserJWT
             .filter { it.isNotEmpty() }
-            .ifPresent { template.header(HttpHeaders.AUTHORIZATION, "Bearer $it") }
+            .ifPresent { template.header(HttpHeaders.AUTHORIZATION, "$PREFIX $it") }
     }
 }
 
