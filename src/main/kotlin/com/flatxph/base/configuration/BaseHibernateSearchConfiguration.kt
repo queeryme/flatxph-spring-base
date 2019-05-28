7730bf9fb55029b7f69b0855ca7424fa8f6074d2
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-05-28 16:45:36 +0800

    Updated Base to 0.0.29 and added configuration for switching to async update of indexes.

--- src/main/kotlin/com/flatxph/base/configuration/BaseHibernateSearchConfiguration.kt
+++ src/main/kotlin/com/flatxph/base/configuration/BaseHibernateSearchConfiguration.kt
@@ -1,6 +1,7 @@
 package com.flatxph.base.configuration
 
 import org.hibernate.search.jpa.Search
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.stereotype.Component
@@ -11,6 +12,9 @@ import javax.persistence.PersistenceContext
 @Component
 abstract class BaseHibernateSearchConfiguration : ApplicationListener<ContextRefreshedEvent> {
 
+    @Value("\${spring.jpa.properties.hibernate.search.async:false}")
+    protected val async: Boolean = false
+
     @PersistenceContext
     protected val entityManager: EntityManager? = null
 
@@ -19,7 +23,8 @@ abstract class BaseHibernateSearchConfiguration : ApplicationListener<ContextRef
         val fullTextEntityManager = Search.getFullTextEntityManager(entityManager)
         try {
             println("Started Creating Index for Hibernate Search")
-            fullTextEntityManager.createIndexer().startAndWait()
+            val indexer = fullTextEntityManager.createIndexer()
+            indexer.apply { if (async) start() else startAndWait() }
             println("Done Creating Index for Hibernate Search")
         } catch (e: InterruptedException) {
             println("Error occurred trying to build Hibernate Search indexes $e")
