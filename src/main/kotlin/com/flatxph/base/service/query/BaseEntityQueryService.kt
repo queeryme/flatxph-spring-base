e57364b367ec5268a81f6961bf27e616d5c76f5c
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-06-17 16:22:08 +0800

    Added child reference criteria specification.

--- src/main/kotlin/com/flatxph/base/service/query/BaseEntityQueryService.kt
+++ src/main/kotlin/com/flatxph/base/service/query/BaseEntityQueryService.kt
@@ -7,21 +7,32 @@ import com.flatxph.base.domain.BaseEntity
 import com.flatxph.base.domain.BaseEntity_
 import com.flatxph.base.dto.BaseDTO
 import io.github.jhipster.service.QueryService
+import io.github.jhipster.service.filter.RangeFilter
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.domain.Specification
+import javax.persistence.metamodel.SetAttribute
+import javax.persistence.metamodel.SingularAttribute
 import com.flatxph.base.criteria.BaseEntityCriteria as Criteria
 import com.flatxph.base.mapper.BaseEntityMapper as Mapper
 import com.flatxph.base.repository.BaseEntityRepository as Repository
 
 abstract class BaseEntityQueryService<E : BaseEntity, C : Criteria>(val repository: Repository<E>) : QueryService<E>() {
 
+    protected fun <OTHER> buildBaseReferringSpecification(
+            filter: RangeFilter<Long>,
+            reference: SetAttribute<E, OTHER>,
+            valueField: SingularAttribute<in OTHER, Long>): Specification<E> {
+        return buildReferringEntitySpecification<OTHER, E, Long>(filter, { root -> root.join(reference) }, { entity -> entity.get(valueField) })
+    }
+
     fun findByCriteria(criteria: C?): List<E> = repository.findAll(createSpecification(criteria))
 
     fun <D : BaseDTO> findByCriteria(mapper: Mapper<D, E>, criteria: C?): List<D> = mapper.toDto(findByCriteria(criteria))
 
-    fun findPagedByCriteria(criteria: C?, page: Pageable?): Page<E> = repository.findAll(createSpecification(criteria), page ?: PageRequest.of(0, ApplicationConstants.PAGE_SIZE))
+    fun findPagedByCriteria(criteria: C?, page: Pageable?): Page<E> = repository.findAll(createSpecification(criteria), page
+            ?: PageRequest.of(0, ApplicationConstants.PAGE_SIZE))
 
     fun <D : BaseDTO> findPagedByCriteria(mapper: Mapper<D, E>, criteria: C?, page: Pageable?): Page<D> = findPagedByCriteria(criteria, page).map { mapper.toDto(it) }
 
