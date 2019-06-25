1554d2dec2c30d0dd920b1fed2f5eebc1660ef65
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-06-25 10:08:22 +0800

    Converted buildBaseReferringSpecification to buildSpecification on most files.

--- src/main/kotlin/com/flatxph/base/service/query/BaseEntityQueryService.kt
+++ src/main/kotlin/com/flatxph/base/service/query/BaseEntityQueryService.kt
@@ -7,25 +7,16 @@ import com.flatxph.base.domain.BaseEntity
 import com.flatxph.base.domain.BaseEntity_
 import com.flatxph.base.dto.BaseDTO
 import io.github.jhipster.service.QueryService
-import io.github.jhipster.service.filter.RangeFilter
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.domain.Specification
-import javax.persistence.metamodel.SetAttribute
-import javax.persistence.metamodel.SingularAttribute
 import com.flatxph.base.criteria.BaseEntityCriteria as Criteria
 import com.flatxph.base.mapper.BaseEntityMapper as Mapper
 import com.flatxph.base.repository.BaseEntityRepository as Repository
 
-abstract class BaseEntityQueryService<E : BaseEntity, C : Criteria>(val repository: Repository<E>) : QueryService<E>() {
 
-    protected fun <OTHER> buildBaseReferringSpecification(
-            filter: RangeFilter<Long>,
-            reference: SetAttribute<E, OTHER>,
-            valueField: SingularAttribute<in OTHER, Long>): Specification<E> {
-        return buildReferringEntitySpecification<OTHER, E, Long>(filter, { root -> root.join(reference) }, { entity -> entity.get(valueField) })
-    }
+abstract class BaseEntityQueryService<E : BaseEntity, C : Criteria>(val repository: Repository<E>) : QueryService<E>() {
 
     fun findByCriteria(criteria: C?): List<E> = repository.findAll(createSpecification(criteria))
 
